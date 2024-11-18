c631a44d86956b51da9f16b25a6502e6880dcba8
Matys <matysallanet@gmail.com>
2024-11-18 14:43:00 +0100

    Création de la méthode pour lister les instruments + la vue

---
+++ src/Controller/InstrumentController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Instrument;
+use Symfony\Component\HttpFoundation\Request;
+
+class InstrumentController extends AbstractController
+{
+    //#[Route('/cours', name: 'app_cours')]
+    public function index(): Response
+    {
+        return $this->render('instrument/index.html.twig', [
+            'controller_name' => 'InstrumentController',
+        ]);
+    }
+
+    public function listerInstrument(ManagerRegistry $doctrine){
+
+        $repository = $doctrine->getRepository(Instrument::class);
+
+        $cours= $repository->findAll();
+        return $this->render('instrument/lister.html.twig', [
+            'pInstruments' => $cours,]);	
+            
+    }
+}
