1c58ad7c6f491ffae3f7be3e68980372b84d8d9c
Théo <totapplecraft@gmail.com>
2024-12-02 11:56:13 +0100

    Ajout de la fonction AjouterContratPret et ajout d'info sur les contrat dans ListerContratPret

--- src/Controller/ContratPretController.php
+++ src/Controller/ContratPretController.php
@@ -73,4 +73,47 @@ class ContratPretController extends AbstractController
                 return $this->render('contratPret/ajouter.html.twig', array('form' => $form->createView(),));
         }
     }
+
+
+
+/*
+    #[Route('/contratPret/modifier/{id}', name: 'app_contratPret_modifier')]
+    public function modifierContratPret(ManagerRegistry $doctrine, $id, Request $request){
+ 
+        $contratPret = $doctrine->getRepository(ContratPret::class)->find($id);
+     
+        if (!$contratPret) {
+            throw $this->createNotFoundException('Aucun contratPret trouvé avec le numéro '.$id);
+        }
+        else
+        {
+                $form = $this->createForm(ContratPretModifierType::class, $contratPret);
+                $form->handleRequest($request);
+     
+                if ($form->isSubmitted() && $form->isValid()) {
+     
+                     $contratPret = $form->getData();
+                     $entityManager = $doctrine->getManager();
+                     $entityManager->persist($contratPret);
+                     $entityManager->flush();
+                     return $this->render('contratPret/consulter.html.twig', ['contratPret' => $contratPret,]);
+               }
+               else{
+                    return $this->render('contratPret/ajouter.html.twig', array('form' => $form->createView(),));
+               }
+            }
+     }
+    
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 }
---
+++ src/Form/ContratPretType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ContratPret;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+
+
+class ContratPretType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('DateDebut', DateType::class)
+            ->add('DateFin', DateType::class)
+            ->add('Description', TextType::class,)
+            ->add('Prix', MoneyType::class,)
+            ->add('Quotite', MoneyType::class,)
+
+            ->add('Eleve', EntityType::class, array('class' => 'App\Entity\Eleve','choice_label' => 'prenom' ))
+            ->add('Instrument', EntityType::class, array('class' => 'App\Entity\Instrument','choice_label' => 'numSerie' ))
+
+            ->add('enregistrer', SubmitType::class, [
+                'label' => 'Nouveaux Contrat de pret'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContratPret::class,
+        ]);
+    }
+}
