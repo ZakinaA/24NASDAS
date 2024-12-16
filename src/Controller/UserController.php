3d6ef3344385dac58ff0a73db1a2caac08483dd4
Matys <matysallanet@gmail.com>
2024-12-16 14:22:31 +0100

    CRUD users ; un admin peut modifier et supprimer un responsable derrière un utilisateur, il peut aussi accéder à la liste des élèves gérés par ce responsable.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -22,7 +22,7 @@ class UserController extends AbstractController
     public function listerUsers(ManagerRegistry $doctrine){
 
         $repository = $doctrine->getRepository(User::class);
-        
+
         $user = $this->getUser();
 
         // Vérifier si un utilisateur es    t connecté et récupérer le responsable
@@ -33,5 +33,29 @@ class UserController extends AbstractController
             'pUsers' => $users,
             'responsable' => $responsable,]);	
             
+    }
+
+    #[Route('/user/consulter/{id}', name: 'app_user_consulter')]
+    public function consulterUser(ManagerRegistry $doctrine, int $id)
+    {
+        $user = $this->getUser();
+
+        // Vérifier si l'utilisateur a un responsable associé
+        $responsable = $user->getResponsable();
+        
+        // Récupérer l'instrument par son ID
+        $users = $doctrine->getRepository(User::class)->find($id);
+
+        if (!$users) {
+            throw $this->createNotFoundException(
+                'Aucun utilisateur trouvé avec le numéro ' . $id
+            );
+        }
+
+        // Passer l'instrument et la date formatée à la vue
+        return $this->render('user/consulter.html.twig', [
+            'users' => $users,
+            'responsable' => $responsable
+        ]);
     }
 }
